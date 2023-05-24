27e02a8ea02a9e4ad044c9db65dd4d9a964f9efb
ShadowoftheRedFox <leon.merin@gmail.com>
2023-05-24 20:14:52 +0200

    fixed color bugs on linux

--- src/main.c
+++ src/main.c
@@ -53,32 +53,26 @@ int main(int argc, char const *argv[]) {
     player_x[3] = 2;
     player_y[3] = MAP_SIZE - 1;
 
-    // TODO load and save player score and name
     // TODO best score leaderboard?
-    // TODO count round
-
-    // TEST -------------------------------
-    save_score("gg", 3, 5, 1);
-    return 0;
 
     // runs menu
     while (menu_running) {
         if (actual_menu == MENU_MAIN) {
             // we are in the main menu
-            printf("    MENU\n    1 - LOAD GAME\n    2 - NEW GAME\n    3 - SCORES\n    4 - QUIT\n");
+            printf("MENU\n1 - LOAD GAME\n2 - NEW GAME\n3 - SCORES\n4 - QUIT\n");
             do {
                 printf(">> ");
                 correct = scanf("%d", &menu_choice);
             } while (menu_choice < 1 || menu_choice > 4 || correct != 1);
             switch (menu_choice) {
             case 1: // load actual game
-                // TODO check if there is a save
+                // check if there is a save
                 if (!save_file_exists()) {
                     printf("There is no game to resume.\n");
                     actual_menu = MENU_MAIN;
                 } else {
-                    load_game(map, &player_number, treasure_found, monster_killed, round_number,
-                              treasure, will_teleport, artifact_found,
+                    load_game(map, &player_number, treasure_found, monster_killed,
+                              round_number, treasure, will_teleport, artifact_found,
                               player_x, player_y, &active_weapon, player_class,
                               player_name, &turn, &is_winner);
                     game_running = true;
@@ -87,7 +81,10 @@ int main(int argc, char const *argv[]) {
                 break;
             case 2: // new game
                 map_setup(map);
-                reset_variables(&player_number, treasure_found, monster_killed, round_number, treasure, will_teleport, artifact_found, player_x, player_y, &active_weapon, player_class, player_name, &turn, &is_winner);
+                reset_variables(&player_number, treasure_found, monster_killed,
+                                round_number, treasure, will_teleport, artifact_found,
+                                player_x, player_y, &active_weapon, player_class,
+                                player_name, &turn, &is_winner);
                 // launch the new game
                 new_game(&player_number, player_name, player_class);
                 // save the score of each player
@@ -98,7 +95,7 @@ int main(int argc, char const *argv[]) {
                 game_running = true;
                 break;
             case 3: // score
-                    // TODO PRINT SCORE
+                    // TODO if there is no score.save, just say there is no score
                 show_score(5);
 
                 break;
@@ -110,16 +107,20 @@ int main(int argc, char const *argv[]) {
         } else if (actual_menu == MENU_GAME) {
             // runs the game for the round
             while (game_running) {
-                // [turn - PLAYER_BLUE] gives the correct number in the array depending of the turn, whatever is the number of player
+                // [turn - PLAYER_BLUE] gives the correct number in the array depending
+                // of the turn, whatever is the number of player
                 turn_number = turn - PLAYER_BLUE;
                 // count the round for each players
                 round_number[turn_number]++;
-                save_game(map, player_number, treasure_found, monster_killed, round_number, treasure, will_teleport, artifact_found,
-                          player_x, player_y, active_weapon, player_class, player_name, turn, is_winner);
+                save_game(map, player_number, treasure_found, monster_killed,
+                          round_number, treasure, will_teleport, artifact_found,
+                          player_x, player_y, active_weapon, player_class, player_name,
+                          turn, is_winner);
 
                 if (is_winner) {
                     game_win(turn, player_name[turn_number], round_number[turn_number]);
-                    save_score(player_name[turn_number], treasure_found[turn_number], monster_killed[turn_number], 1);
+                    save_score(player_name[turn_number], treasure_found[turn_number],
+                               monster_killed[turn_number], 1);
                     u32 answer;
                     printf("Do you want to play a new game?\n1 - Yes\n2 - No\n");
                     do {
@@ -136,7 +137,10 @@ int main(int argc, char const *argv[]) {
                     } else {
                         printf("Readying up to make a new game...\n");
                         map_setup(map);
-                        reset_variables(&player_number, treasure_found, monster_killed, round_number, treasure, will_teleport, artifact_found, player_x, player_y, &active_weapon, player_class, player_name, &turn, &is_winner);
+                        reset_variables(&player_number, treasure_found, monster_killed,
+                                        round_number, treasure, will_teleport,
+                                        artifact_found, player_x, player_y, &active_weapon,
+                                        player_class, player_name, &turn, &is_winner);
                         // launch the new game
                         new_game(&player_number, player_name, player_class);
                         // save the score of each player
@@ -179,17 +183,21 @@ int main(int argc, char const *argv[]) {
 
                 // await player move
                 if (!will_teleport[turn_number]) {
-                    player_move(map, &turn, player_number, &player_x[turn_number], &player_y[turn_number],
-                                &treasure[turn_number], &artifact_found[turn_number]);
+                    player_move(map, &turn, player_number, &player_x[turn_number],
+                                &player_y[turn_number], &treasure[turn_number],
+                                &artifact_found[turn_number]);
                 } else {
                     will_teleport[turn_number] = false;
-                    player_teleport(map, &turn, &player_x[turn_number], &player_y[turn_number]);
+                    player_teleport(map, &turn, &player_x[turn_number],
+                                    &player_y[turn_number]);
                 }
 
                 // logic after the move
-                game_logic(map, &turn, &treasure[turn_number], &monster_killed[turn_number], &artifact_found[turn_number],
+                game_logic(map, &turn, &treasure[turn_number],
+                           &monster_killed[turn_number], &artifact_found[turn_number],
                            player_class[turn_number], active_weapon, player_number,
-                           &will_teleport[turn_number], &player_x[turn_number], &player_y[turn_number], &is_winner);
+                           &will_teleport[turn_number], &player_x[turn_number],
+                           &player_y[turn_number], &is_winner);
             }
         }
     }
--- src/platform_linux.c
+++ src/platform_linux.c
@@ -11,108 +11,129 @@
 
 void platform_sleep(u64 ms) {
 #if _POSIX_C_SOURCE >= 199309L
-    struct timespec ts;
-    ts.tv_sec = ms / 1000;
-    ts.tv_nsec = (ms % 1000) * 1000 * 1000;
-    nanosleep(&ts, 0);
+  struct timespec ts;
+  ts.tv_sec = ms / 1000;
+  ts.tv_nsec = (ms % 1000) * 1000 * 1000;
+  nanosleep(&ts, 0);
 #else
-    if (ms >= 1000) {
-        sleep(ms / 1000);
-    }
-    usleep((ms % 1000) * 1000);
+  if (ms >= 1000) {
+    sleep(ms / 1000);
+  }
+  usleep((ms % 1000) * 1000);
 #endif
 }
 
 void platform_srand() {
-    /* Intializes random number generator */
-    srand((unsigned)time(NULL));
+  /* Intializes random number generator */
+  srand((unsigned)time(NULL));
 }
 
-void platform_console_clear() {
-    printf("\033[H\033[J");
-}
+void platform_console_clear() { printf("\033[H\033[J"); }
 void platform_console_clean() {
-    while (getchar() != '\n') { // clean input
-    }
+  while (getchar() != '\n') { // clean input
+  }
 }
 
-void platform_color_change(Case_Color font, Case_Color back) {
-    // back color
-    switch (back) {
-    case COLOR_RED:
-        back = 41;
-        break;
-    case COLOR_GREEN:
-        back = 42;
-        break;
-    case COLOR_BLUE:
-        back = 44;
-        break;
-    case COLOR_YELLOW:
-        back = 43;
-        break;
-    case COLOR_PINK:
-        back = 45;
-        break;
-    case COLOR_WHITE:
-        back = 47;
-        break;
-    case COLOR_EMPTY:
-        // Specify this one to just pass color, and not make a case for empty cases
-        // back at 0 already
-        break;
-    default:
-        // it's all existing color
-        break;
-    }
-
-    // font color
+void platform_color_change(Case_Color font, Case_Color background) {
+  u32 back = 0;
+  // back color
+  switch (background) {
+  case COLOR_RED:
+    back = 41;
+    break;
+  case COLOR_GREEN:
+    back = 42;
+    break;
+  case COLOR_BLUE:
+    back = 44;
+    break;
+  case COLOR_YELLOW:
+    back = 43;
+    break;
+  case COLOR_PINK:
+    back = 45;
+    break;
+  case COLOR_WHITE:
+    back = 47;
+    break;
+  case COLOR_EMPTY:
+  default:
+    // Specify this one to just pass color, and not make a case for empty cases
     switch (font) {
     case COLOR_RED:
-        printf("\033[31;1;%dm", back);
-        break;
+      printf("\033[31m");
+      break;
     case COLOR_GREEN:
-        printf("\033[32;1;%dm", back);
-        break;
+      printf("\033[32m");
+      break;
     case COLOR_BLUE:
-        printf("\033[34;1;%dm", back);
-        break;
+      printf("\033[34m");
+      break;
     case COLOR_YELLOW:
-        printf("\033[33;1;%dm", back);
-        break;
+      printf("\033[33m");
+      break;
     case COLOR_PINK:
-        printf("\033[35;1;%dm", back);
-        break;
+      printf("\033[35m");
+      break;
     case COLOR_WHITE:
-        printf("\033[37;1;%dm", back);
-        break;
-    case COLOR_EMPTY:
-        // Specify this one to just pass color, and not make a case for empty cases
-        printf("\033[%dm", back);
-        break;
+      printf("\033[37m");
+      break;
     default:
-        // it's all existing color
-        break;
+    case COLOR_EMPTY:
+      // Specify this one to just pass color, and not make a case for empty
+      // cases
+      printf("\033[0m");
+      break;
     }
+    return;
+    break;
+  }
+
+  // font color
+  switch (font) {
+  case COLOR_RED:
+    printf("\033[31;%dm", back);
+    break;
+  case COLOR_GREEN:
+    printf("\033[32;%dm", back);
+    break;
+  case COLOR_BLUE:
+    printf("\033[34;%dm", back);
+    break;
+  case COLOR_YELLOW:
+    printf("\033[33;%dm", back);
+    break;
+  case COLOR_PINK:
+    printf("\033[35;%dm", back);
+    break;
+  case COLOR_WHITE:
+    printf("\033[37;%dm", back);
+    break;
+  case COLOR_EMPTY:
+    // Specify this one to just pass color, and not make a case for empty cases
+    printf("\033[0m");
+    break;
+  default:
+    // it's all existing color
+    break;
+  }
 }
 
 void *platform_allocate(u64 size) {
-    void *r = malloc(size);
-    if (r == NULL) {
-        printf("malloc failed");
-        exit(1);
-    }
+  void *r = malloc(size);
+  if (r == NULL) {
+    printf("malloc failed");
+    exit(1);
+  }
 }
-void platform_free(void *block) {
-    free(block);
-}
+void platform_free(void *block) { free(block); }
 void *platform_zero_memory(void *block, u64 size) {
-    return memset(block, 0, size);
+  return memset(block, 0, size);
 }
 void *platform_copy_memory(void *dest, const void *source, u64 size) {
-    return memcpy(dest, source, size);
+  return memcpy(dest, source, size);
 }
 void *platform_set_memory(void *dest, i32 value, u64 size) {
-    return memset(dest, value, size);
+  return memset(dest, value, size);
 }
 #endif
